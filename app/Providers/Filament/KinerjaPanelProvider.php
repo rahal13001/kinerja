706ee9269091e3cea791b6d36c4e36533654ca96
rahal13001 <rahal13001@mail.unpad.ac.id>
2024-08-12 07:48:09 +0900

    fix bug : dashboard admin

--- app/Providers/Filament/KinerjaPanelProvider.php
+++ app/Providers/Filament/KinerjaPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Dashboard;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -34,7 +35,7 @@ class KinerjaPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-               
+               Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
